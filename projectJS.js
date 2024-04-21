81f9a7f812fbaa93f383bfe04e85407e147cd1f8
hmJ4Zor-Q-3 <129763409+hmJ4Zor-Q-3@users.noreply.github.com>
2024-04-21 19:43:43 -0400

    Add plan collection.

--- projectJS.js
+++ projectJS.js
@@ -22,7 +22,7 @@ function generatePlanner(e){
     $("#display-section").append(headFor($("#name-field").val(), $("#email-field").val(), $("#goal-field").val()));
 
     // add ability to add meals for week
-    let hDVolume = $("<div>").attr("class", "horizontal-scroll-volume");
+    let hDVolume = $("<div>").attr("class", "horizontal-scroll-volume").attr("id", "plans");
     ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].forEach((d) => hDVolume.append(dailyPlannerFor(d)));
     $("#display-section").append(hDVolume);
 }
@@ -37,8 +37,8 @@ function headFor(nam, email, goal){
 } // end headFor
 
 function dailyPlannerFor(day){
-    dP = $("<div>").addClass("form")
-    .append($("<h2>").html(day))
+    dP = $("<div>").addClass("form").attr("id", day.toLowerCase())
+    .append($("<h2>").addClass("_title").html(day))
     .append($("<hr>"));
 
     [{id: `${day.toLowerCase()}_breakfast`, name: "Breakfast"}, 
@@ -70,8 +70,37 @@ function finalizePlanner(){
     $("#clear-planner").parent().toggle();
     $("#clear-plan").parent().toggle();
 
-    // convert to final page like printable format
-}
+    // unfortunely jquery isn't this powerful
+    // let plans = $("#plans > div") // every div right under plans
+    // .map((x) => $(`#${x.attr("id")} input`)) // for each div get all inputs that're under it
+    // .map((v, l) => 
+    // [{
+    //     title: $(`label[for=${v.attr("id")}]`).html(), 
+    //     plan: v.val()
+    // }][0]); // get each input's value and label as an object
+
+    let plans = Array.from(document.getElementById("plans").children)
+    .map((x) => Array.from(x.children))
+    .map((x) => 
+    [{
+        title: x.find((y) => y.classList.contains("_title")).innerHTML, 
+        plans: x.filter((y) => y.nodeName == "INPUT")
+    }][0]);
+    
+    plans.forEach((x) => x.plans = x.plans.map((y) =>
+    [{
+        title: $(`label[for=${y.id}]`).html(), 
+        plan: y.value
+    }][0]));
+
+    $("#plans").remove();
+    console.log(plans)
+    plans.forEach((p) => console.log(`print the plan: ${p}`))
+} // end finalizePlanner()
+
+function dailyPlanFor(){
+
+} // end dailyPlanFor()
 
 
 
